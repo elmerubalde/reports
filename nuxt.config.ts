b627c37cb6e891defa092731ce3679ac4871377e
elmer <elmer.ubalde@gmail.com>
2026-05-09 11:41:04 -0400

    google id

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://jil-cms.canadacentral.cloudapp.azure.com:8080:',
-      googleClientId: '102141595773-j5945pt7t100bm59je2ko17ga4n5culu.apps.googleusercontent.com'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://jil-cms.canadacentral.cloudapp.azure.com:8080',
+      googleClientId: process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID || '102141595773-j5945pt7t100bm59je2ko17ga4n5culu.apps.googleusercontent.com'
     }
   },
   css: ['~/assets/css/tailwind.css'],
